ecc4724e1811745cf4b1f8338f425a438d9f1f74
Michael Schuster <michael.schuster@quartett-mobile.de>
2017-03-06 16:13:10 +0100

    Update greyscale to rgb script to take input directory

--- FuseNet/utils/greyscale_to_rgb.py
+++ FuseNet/utils/greyscale_to_rgb.py
@@ -7,9 +7,9 @@ from PIL import Image
 FLAGS = None
 
 parser = argparse.ArgumentParser(description = 'Convert Grayscale image to RGB image')
-parser.add_argument('--input_path', required = True, help = 'Path to input image')
-parser.add_argument('--cmap_path', required = True, help = 'Path to cmap file')
-parser.add_argument('--output_path', help = 'Path where output image gets saved')
+parser.add_argument('--input_path', help = 'Path to input image')
+parser.add_argument('--input_dir', help = 'Input directory, all files are converted')
+parser.add_argument('--cmap_path', help = 'Path to cmap file', default='../../Raw/cmap.txt')
 FLAGS, unparsed = parser.parse_known_args()
 
 def one_hot(input):
@@ -26,26 +26,22 @@ def get_cmap():
 
     return np.array(values)
 
-cmap = get_cmap()
-
-with open(FLAGS.input_path, 'r') as input_image:
-    grey_image = np.asarray(Image.open(input_image))
+def convert_image(input_path):    
+    with open(input_path, 'r') as input_image:
+        grey_image = np.asarray(Image.open(input_image))
     
-input_shape = grey_image.shape
-grey_image = grey_image.reshape([-1])
-grey_image = one_hot(grey_image)
-rgb_image = grey_image.dot(cmap)
-rgb_image = rgb_image * 256
-rgb_image = rgb_image.astype(np.uint8)
-output_shape = input_shape + (3,)
-rgb_image = rgb_image.reshape(output_shape)
-output = Image.fromarray(rgb_image)
+        input_shape = grey_image.shape
+        grey_image = grey_image.reshape([-1])
+        grey_image = one_hot(grey_image)
+        rgb_image = grey_image.dot(cmap)
+        rgb_image = rgb_image * 256
+        rgb_image = rgb_image.astype(np.uint8)
+        output_shape = input_shape + (3,)
+        rgb_image = rgb_image.reshape(output_shape)
+        output = Image.fromarray(rgb_image)
 
-if FLAGS.output_path is not None:
-    output_path = FLAGS.output_path
-else:
-    directory = os.path.dirname(FLAGS.input_path)
-    filename = os.path.basename(FLAGS.input_path)
+    directory = os.path.dirname(input_path)
+    filename = os.path.basename(input_path)
     name = os.path.splitext(filename)[0]
     extension = os.path.splitext(filename)[1]
 
@@ -53,4 +49,17 @@ else:
 
     output_path = os.path.join(directory, output_filename)
     
-output.save(output_path)
+    output.save(output_path)
+
+
+cmap = get_cmap()
+
+if FLAGS.input_path is not None:
+    convert_image(FLAGS.input_path)
+
+if FLAGS.input_dir is not None:
+    for f in os.listdir(FLAGS.input_dir):
+        if f.endswith('png') or f.endswith('jpg') or f.endswith('jpeg'):
+            file_path = os.path.join(FLAGS.input_dir, f)
+            convert_image(file_path)
+   
